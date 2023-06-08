6ea6c80b1a97c01f06d113b034c1f0dc82b6b1af
kkich <129613785+kkich@users.noreply.github.com>
2023-06-09 00:05:03 +0600

    I create canvas and context variables that get a reference to the canvas element and its drawing context. Variables are defined for the grid size, the counter, and the delay between screen updates. The snake object is initialized with the initial coordinates, the direction of movement and an array of cells for the snake. The apple object is initialized with the initial coordinates for the apple. I wrote a loop function that updates the state of the game and draws elements on canvas. I assign a keydown event handler to respond to keystrokes and changes in the direction of movement of the snake. Starting the game cycle.

---
+++ script.js
@@ -0,0 +1,105 @@
+var canvas = document.getElementById('game');
+var context = canvas.getContext('2d');
+var grid = 16;
+var count = 0;
+var delay = 80;
+var score = 0;
+
+var snake = {
+  // Начальные координаты
+  x: 160,
+  y: 160,
+
+  dx: grid,
+  dy: 0,
+
+  cells: [],
+
+  maxCells: 4
+};
+
+var apple = {
+  // Начальные координаты яблока
+  x: 320,
+  y: 320
+};
+var score = 0;
+
+function loop() {
+    requestAnimationFrame(loop);
+    if (++count < delay / 4) {
+        return;
+    }
+    count = 0;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    snake.x += snake.dx;
+    snake.y += snake.dy;
+    if (snake.x < 0) {
+        snake.x = canvas.width - grid;
+    }
+    else if (snake.x >= canvas.width) {
+        snake.x = 0;
+    }
+    if (snake.y < 0) {
+        snake.y = canvas.height - grid;
+    }
+    else if (snake.y >= canvas.height) {
+        snake.y = 0;
+    }
+    snake.cells.unshift({ x: snake.x, y: snake.y });
+    if (snake.cells.length > snake.maxCells) {
+        snake.cells.pop();
+    }
+    context.fillStyle = 'red';
+    context.fillRect(apple.x, apple.y, grid - 1, grid - 1);
+    context.fillStyle = 'green';
+    snake.cells.forEach(function (cell, index) {
+        context.fillRect(cell.x, cell.y, grid - 1, grid - 1);
+        if (cell.x === apple.x && cell.y === apple.y) {
+            snake.maxCells++;
+            score++;
+            document.getElementById('score').textContent = score;
+
+            apple.x = getRandomInt(0, 25) * grid;
+            apple.y = getRandomInt(0, 25) * grid;
+        }
+        for (var i = index + 1; i < snake.cells.length; i++) {
+            if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
+                snake.x = 160;
+                snake.y = 160;
+                snake.cells = [];
+                snake.maxCells = 4;
+                snake.dx = grid;
+                snake.dy = 0;
+                apple.x = getRandomInt(0, 25) * grid;
+                apple.y = getRandomInt(0, 25) * grid;
+                score = 0;
+            }
+        }
+    });
+
+    context.fillStyle = 'white';
+    context.font = '20px Arial';
+    context.fillText('Score: ' + score, 10, 20);
+}
+
+document.addEventListener('keydown', function (e) {
+    if (e.which === 37 && snake.dx === 0) {
+        snake.dx = -grid;
+        snake.dy = 0;
+    }
+    else if (e.which === 38 && snake.dy === 0) {
+        snake.dy = -grid;
+        snake.dx = 0;
+    }
+    else if (e.which === 39 && snake.dx === 0) {
+        snake.dx = grid;
+        snake.dy = 0;
+    }
+    else if (e.which === 40 && snake.dy === 0) {
+        snake.dy = grid;
+        snake.dx = 0;
+    }
+});
+
+requestAnimationFrame(loop);
